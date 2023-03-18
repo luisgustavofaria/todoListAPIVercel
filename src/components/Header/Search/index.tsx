aea768af2400c4de7cbc0a2094a6b29e5637b681
Luis Gustavo Faria <luis.gustavo.fariac@gmail.com>
2023-03-18 11:28:37 -0300

    header filter ok

--- src/components/Header/Search/index.tsx
+++ src/components/Header/Search/index.tsx
@@ -1,12 +1,22 @@
 import Image from "next/image"
+import { useState } from "react";
 
 import search from "../../../../public/search.svg"
 import { Button, ContainerInput, Input } from "./styles"
 
 export default function Search(  ) {
+
+  const [searchValue, setSearchValue] = useState('');
+  
+  
     return (
       <ContainerInput>
-        <Input type="text" placeholder="Pesquisar Notas" />
+        <Input 
+          type="text" 
+          placeholder="Pesquisar Notas" 
+          value={searchValue}     
+          onChange={(event) => setSearchValue(event.target.value)}
+          />
         
         <Image src={search} alt="vectorX" />
         
