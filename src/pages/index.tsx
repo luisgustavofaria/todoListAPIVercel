033254b9199ba093f234a80f3c6f00803d203bc2
Luis Gustavo Faria <luis.gustavo.fariac@gmail.com>
2023-03-19 15:02:39 -0300

    created post, put, update, delete

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,6 +6,7 @@ import Header from "@/components/Header"
 import TodoForm from "@/components/TodoForm"
 import TodoList from "@/components/TodoList";
 import { searchAtom } from "../components/Header/Search";
+import axios from "axios";
 
 
 export interface ITodoList {
@@ -18,43 +19,71 @@ export interface ITodoList {
 export default function Home() {
 
   const [todoForm, setTodoForm] = useState<ITodoList[]>([])
-console.log(todoForm);
 
-  function addTodoList(titleNew:string, textAreaNew: string, isFavorited: boolean) {
-    setTodoForm( oldState => [
-      ...oldState,
-      {
-        id: crypto.randomUUID(),
-        titleTodoList: titleNew,
-        textAreaTodoList: textAreaNew,
-        isFavorited,
-      }
-    ])
-  }
 
-  function deleteTodoListById(todoListId: string){
+async function getTodo() {
+  const response = await axios.get("http://localhost:3333/todo/list-many");
+
+  setTodoForm(response.data);
+}
+
+useEffect(() => {
+  getTodo();
+}, []);
+
+async function addTodoList(
+  titleNew: string,
+  textAreaNew: string,
+  isFavorited: boolean
+) {
+  const data = {
+    id: crypto.randomUUID(),
+    titleTodoList: titleNew,
+    textAreaTodoList: textAreaNew,
+    isFavorited
+  };
+
+  setTodoForm((oldState) => [...oldState, data]);
+
+  await axios.post("http://localhost:3333/todo/create", data);
+}
+
+
+
+  async function deleteTodoListById(todoListId: string){
     const newTodoForm = todoForm.filter((todoList) => todoList.id !== todoListId)
     setTodoForm(newTodoForm)
+    await axios.delete("http://localhost:3333/todo/delete-unique", {
+      params:{todo_id: todoListId}
+    });
   }
 
-  function editTodoListById(todoListId: string, title: string, textarea: string){
+  async function editTodoListById(todoListId: string, title: string, textarea: string){
+    
     const todoListToEdit = todoForm.find(el => el.id === todoListId);
 
     if(todoListToEdit) {
-      const todoListEdited: ITodoList = {
+      const todoListEdited = {
         ...todoListToEdit,
+        todo_id: todoListToEdit.id,
         titleTodoList: title,
         textAreaTodoList: textarea,
       }
       const newTodoList = todoForm.map(el => el.id === todoListId ? todoListEdited : el)
       setTodoForm(newTodoList);
+      await axios.put("http://localhost:3333/todo/update-unique", todoListEdited);
     }
+
+    
   }
 
-
-function toggleFavorited(id: string) {
-  const newTodoForm = todoForm.map((todoList) => {
+  async function toggleFavorited(id: string) {
+  const newTodoForm = todoForm.map( (todoList) => {
       if (todoList.id === id) {
+          axios.put("http://localhost:3333/todo/update-unique", {
+          todo_id: todoList.id,
+          isFavorited: !todoList.isFavorited
+        }).then(r => r) ;
         return {
           ...todoList,
           isFavorited: !todoList.isFavorited,
@@ -63,27 +92,28 @@ function toggleFavorited(id: string) {
       return todoList;
     })
     setTodoForm(newTodoForm)
+   
   }
 
   const searchValue = useAtomValue(searchAtom).toLocaleLowerCase();
   
   const list = useMemo(() => {
+
     const filterTodos = todoForm.filter(
       (task) =>
         `${task.titleTodoList?.toLocaleLowerCase()}${task.textAreaTodoList?.toLocaleLowerCase()}`.includes(
           searchValue
-        )  || !task.isFavorited && task.isFavorited
-    )
+        ) ||
+        (!task.isFavorited && task.isFavorited)
+        
+    );
 
     const filterReorganize = filterTodos.sort((a, b) =>
-        Number(a.isFavorited > b.isFavorited) === 1 ? -1 : 0
-      )
+      Number(a.isFavorited > b.isFavorited) === 1 ? -1 : 0
+    );
 
     return filterReorganize;
-
-  }, [todoForm, searchValue])
-
-  
+  }, [searchValue, todoForm]);
 
   return (
     <div>
